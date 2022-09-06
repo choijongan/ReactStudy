066cf2c842460043f8ac4f7b63bd1b2e32e7cbaf
choijongan <mmm7134@naver.com>
2022-09-06 19:41:23 +0900

    왠만하면 state, 잠깐 기억 값만 변수!

--- jonganproject/first-react/src/App.js
+++ jonganproject/first-react/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import './App.css';
 import Box from './component/Box';
 
@@ -7,9 +7,20 @@ function App() {
   const [counter2, setCounter2] = useState(0);
   const increase =()=>{
     counter = counter + 1;
-    setCounter2(counter2+1);
+    setCounter2(counter2+1); //변수는 동기적, state 바꾸는건 비동기적
     console.log("counter는", counter, "counter2는", counter2);
-  }
+  };
+
+  //1. 유저가 버튼을 클릭한다.
+  //2. counter+1해서 1이됨
+  //3. setState함수호출
+  //4. console.log실행됨
+  //변수값은 1로 보이고 state값은 아직 안변했기 때문에 그전에 값이 보인다.
+  //함수 끝
+  //app이 다시 rerender
+  //let counter = 0을 거치면서 counter 값은 다시 0으로 초기화가 된다
+  //업데이트된 state 값이 보인다.
+
   return (
     <div>
       <div>{counter}</div>
