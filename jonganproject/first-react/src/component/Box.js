86b8fbf18448055d54bfba1fabb69fbe3fa52c68
choijongan <mmm7134@naver.com>
2022-09-06 19:08:41 +0900

    props

--- jonganproject/first-react/src/component/Box.js
+++ jonganproject/first-react/src/component/Box.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Box = () => {
+const Box = (props) => {
+    console.log("props:", props);
   return (
     <div className="Box">
-      Box1
-      <p>리사</p>
+      Box{props.num}
+      <p>{props.name}</p>
     </div>
   );
 };
