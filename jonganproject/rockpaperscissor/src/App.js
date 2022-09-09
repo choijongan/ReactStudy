d499fdffaf8c919584f026c9057cc6884cb56761
choijongan <mmm7134@naver.com>
2022-09-10 00:04:13 +0900

    가위바위보 프로젝트 완성(app.js box타이틀==box.js타이틀 대소문자 주의

--- jonganproject/rockpaperscissor/src/App.js
+++ jonganproject/rockpaperscissor/src/App.js
@@ -26,25 +26,48 @@ const choice = {
 function App() {
   const [userSelect,setUserSelect] = useState(null);
   const [computerSelect,setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
   const play=(userChoice)=>{
     setUserSelect(choice[userChoice]);
-    let computerChoice = randomChoic();
+    let computerChoice = randomChoice();
     setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
   };
 
-  const randomChoic=()=>{
+  const randomChoice =()=>{
     let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수다.
     console.log("item array", itemArray);
-    let randomItem =Math.floor(Math.random() * itemArray.length);
-    let final = itemArray[randomItem]
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    console.log("random value", randomItem);
+    let final = itemArray[randomItem];
     return choice[final];
   };
 
+  const judgement = (user, computer) => {
+    console.log("user",user, "computer", computer);
+
+    // user == computer tie
+    // user == rock, computer == "scissors" user 이긴거지
+    // user == "rock" computer == paper   user 진거지
+    // user == scissors computer paper    user 이긴거지
+    // user == scissors computer rock     user 진거지
+    // user == paper computer rock   user 이긴거지
+    // user paper computer scissors user 진거지
+
+    if(user.name == computer.name) {
+      return "tie";
+    }else if(user.name =="Rock")
+     return computer.name=="Scissors" ? "win" : "lose"; 
+    else if(user.name =="Scissors")
+     return computer.name == "Paper" ? "win" : "lose";
+    else if(user.name == "Paper")
+     return computer.name == "Rock" ? "win" : "lose";
+  };
   return( 
   <div>
     <div className='main'>
-      <Box title="You" item={userSelect}/>
-      <Box title="computer" item={computerSelect}/>
+      <Box title="You" item={userSelect} result={result}/>
+      <Box title="Computer" item={computerSelect} result={result}/>
     </div>
     <div className='main'>
       <button onClick={() => play("scissors")}>가위</button>
