aad24e3196262dc1eb92d286c919a114615480b2
choijongan <mmm7134@naver.com>
2022-09-09 21:20:16 +0900

    컴퓨터 랜덤한 값 뽑기

--- jonganproject/rockpaperscissor/src/App.js
+++ jonganproject/rockpaperscissor/src/App.js
@@ -24,17 +24,27 @@ const choice = {
   },
 };
 function App() {
-  const [userSelect,setUserSelect] = useState(null)
-
+  const [userSelect,setUserSelect] = useState(null);
+  const [computerSelect,setComputerSelect] = useState(null);
   const play=(userChoice)=>{
-    setUserSelect(choice[userChoice])
-
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoic();
+    setComputerSelect(computerChoice);
   };
+
+  const randomChoic=()=>{
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수다.
+    console.log("item array", itemArray);
+    let randomItem =Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem]
+    return choice[final];
+  };
+
   return( 
   <div>
     <div className='main'>
       <Box title="You" item={userSelect}/>
-      {/*<Box title="computer"/>*/}
+      <Box title="computer" item={computerSelect}/>
     </div>
     <div className='main'>
       <button onClick={() => play("scissors")}>가위</button>
